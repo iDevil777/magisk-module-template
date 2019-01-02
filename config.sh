47ecd6eafcd276350cc8ba1f097fe775fba4acb6
iDevil777 <46319573+iDevil777@users.noreply.github.com>
2019-01-02 18:48:04 +0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -28,7 +28,7 @@ AUTOMOUNT=true
 PROPFILE=false
 
 # Set to true if you need post-fs-data script
-POSTFSDATA=false
+POSTFSDATA=true
 
 # Set to true if you need late_start service script
 LATESTARTSERVICE=false
@@ -40,9 +40,11 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
-  ui_print "*******************************"
+  ui_print "**************************************"
+  ui_print "*       SafetyPatcher by iDevil      *"
+  ui_print "* Simply flash the module and reboot *"
+  ui_print "*  More importantly thank me on XDA  *"
+  ui_print "**************************************"
 }
 
 ##########################################################################################
@@ -52,17 +54,10 @@ print_modname() {
 # List all directories you want to directly replace in the system
 # Check the documentations for more info about how Magic Mount works, and why you need this
 
-# This is an example
-REPLACE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
 # Construct your own list here, it will override the example above
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
+/system/bin/safetypatch
 "
 
 ##########################################################################################
@@ -85,6 +80,7 @@ set_permissions() {
 
   # The following is default permissions, DO NOT remove
   set_perm_recursive  $MODPATH  0  0  0755  0644
+  set_perm $MODPATH/system/bin/safetypatch  0 0 0554
 }
 
 ##########################################################################################
